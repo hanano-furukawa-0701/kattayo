05b9e11256611312d4691859ca4a6d7f1af2a7ec
hanano-furukawa-0701 <65842692+hanano-furukawa-0701@users.noreply.github.com>
2020-11-11 22:35:08 +0900

    devise haml1

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   devise_scope :user do
     get 'target_amounts', to: 'users/registrations#new_target_amounts'
     post 'target_amounts', to: 'users/registrations#create_target_amounts'
   end
-  
+
   root 'home#index'
 
   resources :items, only: [:index, :new, :create]
