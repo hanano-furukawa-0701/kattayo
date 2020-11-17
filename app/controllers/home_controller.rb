07fbf7dca050483d13e12d7f0c5abc4f6809cd77
hanano-furukawa-0701 <65842692+hanano-furukawa-0701@users.noreply.github.com>
2020-11-17 22:26:05 +0900

    index1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    @items = Item.limit(5).order('created_at DESC')
+    @categories = Category.limit(5).order('target_amount DESC')
   end
   
 end
