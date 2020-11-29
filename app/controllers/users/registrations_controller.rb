b6c1acb7dce30614302f3017939ab583798cf93f
hanano-furukawa-0701 <65842692+hanano-furukawa-0701@users.noreply.github.com>
2020-11-29 11:18:27 +0900

    user sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,41 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:wassword]
+    @target_amount = @user.build_target_amount
+    render :new_target_amounts
+  end
+
+  def create_target_amount
+    @user = User.new(session["devise.regist_data"]["user"])
+    @target_amount = TargetAmount.new(target_amount_params)
+    unless @target_amount.valid?
+      flash.now[:alert] = @target_amount.errors.full_messages
+      render :new_target_amounts and return
+    end
+    @user.build_target_amount(@target_amount.attributes)
+    @user.save
+    sign_in(:user, @user)
+  end
+
+  protected
+  def target_amount_params
+    params.require(:target_amount).permit(:price)
+  end
+  
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
