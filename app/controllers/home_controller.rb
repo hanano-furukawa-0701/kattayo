3f483bb8a901f55426ec7b657a36500d54a7f39f
hanano-furukawa-0701 <65842692+hanano-furukawa-0701@users.noreply.github.com>
2020-12-07 23:03:04 +0900

    home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @target_amount = TargetAmount.where(user_id: current_user.id).first
-    @current_amount = Item.group("MONTH(date)").sum(:price)
+    # @target_amount = TargetAmount.where(user_id: current_user.id).first
+    # @current_amount = Item.group("MONTH(created_at))").sum(:price)
     @items = Item.limit(5).order('created_at DESC')
     @categories = Category.limit(5).order('target_amount DESC')
   end
