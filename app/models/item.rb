5302509f083e183558dc3180d2834431bcc03854
はなの <nano_3330@icloud.com>
2021-02-02 23:05:30 +0900

    validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  validates :price, presence: true,  format: { with: /\A[0-9]+\z/}
 
   def self.search(search)
     return Item.all unless search
--- app/models/target_amount.rb
+++ app/models/target_amount.rb
@@ -1,4 +1,4 @@
 class TargetAmount < ApplicationRecord
   belongs_to :user, optional: true
-  validates :price, presence: true
+  validates :price, presence: true,  format: { with: /\A[0-9]+\z/}
 end
